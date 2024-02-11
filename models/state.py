35a488a5abd72ad1c7bbda70c626fbd64661d737
Nnadi Chukwuka <chukwukannadi@yahoo.com>
2024-02-10 18:03:32 -0800

    my first contribution to airbnb

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is the Class that defines a state
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class to create a state"""
+    name = ""
